1866b09a81f881584f263349c13ca3e3ef3828e6
Nicolas Perrenoud <mail@nicu.ch>
2022-04-11 09:29:30 +0300

    date welcome

--- app2.py
+++ app2.py
@@ -1,18 +1,19 @@
 import os
+from unicodedata import name
 from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+import re
 
 app = Flask(__name__)
+
+navigation = [{"href": "/", "caption": "Home"},
+              {"href": "/date", "caption": "Date and time"}]
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', name="John Doe")
+    return render_template('index.html', navigation=navigation, name="John Doe")
     # return "<h1>Hello World</h1>"
-
-
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -21,7 +22,31 @@ def login():
         user = request.form['name']
     else:
         user = request.args.get('name')
-    return redirect(url_for('success', name=user))
+    return redirect(url_for('hello_there', name=user))
+
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
+
+
+@app.route("/date")
+def date():
+    now = datetime.now()
+    return now.strftime("%A, %d %B, %Y at %X")
 
 
 if __name__ == '__main__':
